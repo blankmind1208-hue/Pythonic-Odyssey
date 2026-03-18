762825eb49376aabe41e9b3dc28c7a2bd3875301
Bella <blank.mind1208@gmail.com>
2026-03-19 06:33:09 +0800

    Add coding challenges for Day 1

---
+++ Day 1 to 20- Beginner/Day 1/Day_1_Coding_challenges.py
@@ -0,0 +1,28 @@
+#Challenge 1: The Identity Card
+#Write code to print your name, your favorite hobby, and your dream job on three separate lines using only one print() statement.
+
+print("My name is Bella na hindi alam ginagawa\nMy hobby is reading and other things depending on the mood\nI use to dream on becoming a programmer but i got sidetrack")
+
+#Challenge 2: The Debugging Hero
+#Copy this code into your editor and fix the errors so it runs perfectly:
+#`print(Day 1 - Python Print Function")
+#print("The function is declared like this: print('what to print')
+#  print("Hello" + "World")`
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this: print('what to print')")
+print("Hello" + "World")
+
+
+# Challenge 3: The Text Artist
+#Use multiple `print` statements to draw a simple "face" or "shape" in the console. For example:
+#Plaintext
+#  ___
+# (o o)
+#(  V  )
+#--m-m--
+
+print(" ", "---")
+print(" (o o) ")
+print(" ( V )")
+print("--m-m--")
