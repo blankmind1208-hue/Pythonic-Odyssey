26fb32e065eb1f217d2742af0cfe32a6592a522d
Bella <blank.mind1208@gmail.com>
2026-03-29 22:00:04 +0800

    Add Suzanne's password for secure login

---
+++ Day 1 to 20- Beginner/Day 1/Day_6_files.md/Add_a_password.py
@@ -0,0 +1,23 @@
+#👉 In the code below the username and password must both be correct for Mark to login.
+#print("SECURE LOGIN")
+#username = input("Username > ")
+#password = input("Password> ")
+#if username == "mark" and password == "password":
+#print("Welcome Mark!")
+#elif username == "suzanne":
+#print("Hey there Suzanne!")
+#else:
+#print("Go away!")
+ 
+#👉 Suzanne's password is 'Su74nne'. Can you add that to the code above? 
+#(HINT: It should be very similar to Mark.)
+
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input("Password> ")
+if username == "mark" and password == "password":
+print("Welcome Mark!")
+elif username == "suzanne" and password == "Su74nne":
+print("Hey there Suzanne!")
+else:
+print("Go away!")
