7c922671462589c081283026a7c47e5ef6cae086
Bella <blank.mind1208@gmail.com>
2026-03-29 21:55:44 +0800

    Add elif statement for username 'suzanne' in login
    Refactor secure login code to include an elif statement for 'suzanne'.

---
+++ Day 1 to 20- Beginner/Day 1/Day_6_files.md/Day_6.py
@@ -0,0 +1,22 @@
+#print("SECURE LOGIN")
+#username = input("Username > ")
+#if username == "mark":
+#print("Welcome Mark!")
+#else:
+#print("Go away!")
+
+#Add this elif statement to the code above. 
+#Make sure you indent properly AND put it in between the if and else statements!
+
+#elif username == "suzanne":
+# print("Hey there Suzanne!")
+
+print("SECURE LOGIN")
+username = input("Username > ")
+
+if username == "mark":
+  print("Welcome Mark!")
+elif username == "suzanne":
+  print("Hey there Suzanne!")
+else:
+  print("Go away!")
