d614d8f186415ef4d34ed6f55e1d59d5be56157d
Bella <blank.mind1208@gmail.com>
2026-03-31 22:17:20 +0800

    Add motivational quotes based on the day

---
+++ Day 1 to 20- Beginner/Day 8/1_Day_8_Project_Day.py
@@ -0,0 +1,25 @@
+print("Welcome! I am here to give some motivational quotes for you depending on the day")
+print()
+name = input("Please put your name to log in: ")
+
+if name == "Andrew" or name == "andrew":
+    print("Welcome my bebekwah,", name)
+
+    days = input("What's the day today? ")
+    if days == "Monday" or days == "monday":
+        print(name, "the way you show up every day—with effort and love—already makes you enough. Bella is always thankful of you! Have a great", days)
+    elif days == "Tuesday" or days == "tuesday":
+        print("You don’t just work hard,", name,
+              "you love deeply—and that combination is rare and powerful. Bella greets you a happy ", days)
+    elif days == "Wednesday" or days == "wednesday":
+        print("It's", days, "Your consistency builds the future you dream of, even on the days no one sees it", name)
+    elif days == "Thursday" or days == "thursday":
+        print("A merry", day, name, "The care you give isn’t unnoticed. It’s felt, appreciated, and deeply valued. I'm always thankful for your existence")
+    elif days == "Friday" or days == "friday":
+        print(name, "You’re not just chasing goals—you’re creating a life filled with purpose and love, I am excited to see you")
+    elif days == "Saturday" or days == "saturday":
+        print("This", days, "Being a hardworking man and a loving partner isn’t easy, yet you carry both with quiet strength")
+    elif days == "Sunday" or days == "sunday":
+        print(name, "you are becoming the kind of man people trust, respect, and feel safe with—and that matters more than you think")
+else:
+    print("I am not sure who you are, but I hooe you have a great day!")
